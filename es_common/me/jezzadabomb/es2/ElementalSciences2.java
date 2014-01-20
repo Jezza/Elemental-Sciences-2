3aeeec8cf036cf8b85e1108244e83849ca224437
Jezzadabomb <jezzadabomb@gmail.com>
2014-01-20 19:02:58 +1100

    No capes.

--- es_common/me/jezzadabomb/es2/ElementalSciences2.java
+++ es_common/me/jezzadabomb/es2/ElementalSciences2.java
@@ -1,19 +1,12 @@
 package me.jezzadabomb.es2;
 
-import java.io.File;
-
-import cofh.api.core.RegistryAccess;
-
 import me.jezzadabomb.es2.common.Entities;
 import me.jezzadabomb.es2.common.ModBlocks;
 import me.jezzadabomb.es2.common.ModItems;
-import me.jezzadabomb.es2.common.api.HUDBlackLists;
-import me.jezzadabomb.es2.common.core.CapeRegistry;
 import me.jezzadabomb.es2.common.core.ESLogger;
 import me.jezzadabomb.es2.common.core.config.ConfigHandler;
 import me.jezzadabomb.es2.common.lib.Reference;
 import me.jezzadabomb.es2.common.packets.handler.PacketHandler;
-import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import cpw.mods.fml.common.Mod;
@@ -32,44 +25,43 @@ import cpw.mods.fml.relauncher.SideOnly;
 @NetworkMod(channels = { Reference.CHANNEL_NAME }, clientSideRequired = true, serverSideRequired = true, packetHandler = PacketHandler.class)
 public class ElementalSciences2 {
 
-	@Instance(Reference.MOD_ID)
-	public static ElementalSciences2 instance;
+    @Instance(Reference.MOD_ID)
+    public static ElementalSciences2 instance;
 
-	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
-	public static CommonProxy proxy;
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;
 
-	public static CreativeTabs creativeTab = new CreativeTabs(Reference.MOD_ID) {
-		@Override
-		@SideOnly(Side.CLIENT)
-		public Item getTabIconItem() {
-			return ModItems.glasses;
-		}
-	};
+    public static CreativeTabs creativeTab = new CreativeTabs(Reference.MOD_ID) {
+        @Override
+        @SideOnly(Side.CLIENT)
+        public Item getTabIconItem() {
+            return ModItems.glasses;
+        }
+    };
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		ESLogger.init();
-		ConfigHandler.init(event.getSuggestedConfigurationFile());
-		ModBlocks.init();
-		ModItems.init();
-		proxy.runClientSide();
-		proxy.runServerSide();
-	}
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ESLogger.init();
+        ConfigHandler.init(event.getSuggestedConfigurationFile());
+        ModBlocks.init();
+        ModItems.init();
+        proxy.runClientSide();
+        proxy.runServerSide();
+    }
 
-	@EventHandler
-	public void load(FMLInitializationEvent event) {
-		proxy.registerTileEntities();
-		Entities.init();
-	}
+    @EventHandler
+    public void load(FMLInitializationEvent event) {
+        proxy.registerTileEntities();
+        Entities.init();
+    }
 
-	@EventHandler
-	public void postLoaded(FMLPostInitializationEvent event) {
-		CapeRegistry.init();
-	}
+    @EventHandler
+    public void postLoaded(FMLPostInitializationEvent event) {
+    }
 
-	@EventHandler
-	public void onServerStart(FMLServerStartingEvent event) {
-		proxy.registerTickHandlers();
-	}
+    @EventHandler
+    public void onServerStart(FMLServerStartingEvent event) {
+        proxy.registerTickHandlers();
+    }
 
 }
